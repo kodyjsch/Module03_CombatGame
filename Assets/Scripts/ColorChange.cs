242d3c33e158adaa7b29d8c4b4337b83de4c4639
kodyjsch <dakota.schweitzer04@gmail.com>
2026-02-02 17:28:20 -0600

    Dummy Color Flash

---
+++ Assets/Scripts/ColorChange.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    public Renderer rend;
+    public Color flashColor = Color.red;
+    public float flashDuration = .2f;
+
+    private Color originalColor;
+
+    private void Start()
+    {
+        originalColor = rend.material.color;
+    }
+
+    IEnumerator flash()
+    {
+        rend.material.color = flashColor;
+        yield return new WaitForSeconds(flashDuration);
+        rend.material.color = originalColor;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) { 
+            StartCoroutine(flash());
+        }
+    }
+
+}
